9ed182b109f0d8c963b3e8685d2900088efa528d
granmaster1 <starlinmaster@hotmail.com>
2023-08-13 19:16:22 -0400

    Agregado código de perfil de usuario

--- profile.php
+++ profile.php
@@ -1,1 +1,17 @@
- 
+<?php
+
+session_start();
+
+if(!isset($_SESSION['id'])) {
+  header("Location: login.php");
+  exit();
+}
+
+// Mostrar datos del usuario
+echo "<h1>Bienvenido " . $_SESSION['name'] . "</h1>";
+echo "<p>Email: " . $_SESSION['email'] . "</p>";
+echo "<p>ID: " . $_SESSION['id'] . "</p>";
+
+// Cerrar sesión
+session_destroy();
+header("Location: login.php");

5d5ec279fbbdcbff3ab047f656433d06e81e03d9
granmaster1 <starlinmaster@hotmail.com>
2023-08-13 15:07:15 -0400

    Agregada encriptación y redirección a profile.php después de login

--- login.php
+++ login.php
@@ -1,54 +1,42 @@
 <?php
 
-// Incluimos el archivo de conexión a la BD
+// Conexión BD 
 require 'db.php';
 
-// Iniciamos la sesión
-session_start();
+session_start(); 
 
-// Verificamos si se envió el formulario
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-  // Obtenemos los datos del formulario
+  // Obtener datos
   $email = $_POST['email'];
-  $password = $_POST['password'];
+  $password = $_POST['password'];   
 
-  // Consulta SQL para obtener el usuario
+  // Consulta SQL
   $sql = "SELECT * FROM usuarios WHERE email = ?";
-  
+
   $stmt = mysqli_prepare($conn, $sql);
   mysqli_stmt_bind_param($stmt, "s", $email);
   mysqli_stmt_execute($stmt);
-  
+
   $result = mysqli_stmt_get_result($stmt);
 
   $user = mysqli_fetch_assoc($result);
 
-  // Verificamos que la contraseña sea correcta
+  // Verificar contraseña 
   if(password_verify($password, $user['password'])) {
-     // Contraseña correcta, código para iniciar sesión
+      
+    // Iniciar sesión
+    $_SESSION['id'] = $user['id'];
+    $_SESSION['name'] = $user['name'];
 
-  } else {
-     // Contraseña inválida
+    // Redireccionar a perfil (NUEVO)
+    header("Location: profile.php");
+  }
+  else {
+    // Contraseña inválida
   }
 
 }
 
-// Código HTML del formulario de login
+// Formulario
 ?>
-
-<!DOCTYPE html>
-<html>
-<head></head>
-<body>
-
-  <h1>Iniciar Sesión</h1>
-
-  <form method="POST" action="login.php">
-    <input type="email" name="email">
-    <input type="password" name="password"> 
-    <button type="submit">Ingresar</button>
-  </form>
-
-</body>
-</html>
